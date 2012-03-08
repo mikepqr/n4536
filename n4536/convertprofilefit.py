2e5c6c389584868e5ddcb202bdf9433e8fd29fc2
Michael Williams <mike@pentangle.net>
2012-03-08 18:30:05 +0100

    convert to NICMOS scale/zp

--- n4536/convertprofilefit.py
+++ n4536/convertprofilefit.py
@@ -1,8 +1,10 @@
 import imfit
 import os, copy
 
-pix = 0.75
-zp = 18.1639
+iracpix = 0.75
+iraczp = 18.1639
+nicmospix = 0.075
+nicmoszp = 15.1304
 
 def convertprofilefit(d = "~/research/2012/smbhpb/data/n4536/profilefit/", \
         root = "n4536_nicmosirac1_pa120.0_w1"):
@@ -12,15 +14,26 @@ def convertprofilefit(d = "~/research/2012/smbhpb/data/n4536/profilefit/", \
 
     funcs, params = imfit.ReadConfigFile(infile)
 
-    params_converted = copy.deepcopy(params)
+    params_irac = copy.deepcopy(params)
     # Sersic surface brightness to counts
-    params_converted[0][2] = 10**(0.4 * (zp - params[0][2]))
+    params_irac[0][2] = 10**(0.4 * (iraczp - params[0][2]))
     # Exponential surface brightness to counts
-    params_converted[1][1] = 10**(0.4 * (zp - params[1][1]))
+    params_irac[1][1] = 10**(0.4 * (iraczp - params[1][1]))
     # Sersic scale to arcseconds
-    params_converted[0][3] = params[0][3]/pix
+    params_irac[0][3] = params[0][3]/iracpix
     # Exponential surface brightness to counts
-    params_converted[1][2] = params[1][2]/pix
+    params_irac[1][2] = params[1][2]/iracpix
+
+    params_nicmos = copy.deepcopy(params)
+    # Sersic surface brightness to counts
+    params_nicmos[0][2] = 10**(0.4 * (nicmoszp - params[0][2]))
+    # Exponential surface brightness to counts
+    params_nicmos[1][1] = 10**(0.4 * (nicmoszp - params[1][1]))
+    # Sersic scale to arcseconds
+    params_nicmos[0][3] = params[0][3]/nicmospix
+    # Exponential surface brightness to counts
+    params_nicmos[1][2] = params[1][2]/nicmospix
 
     print params
-    print params_converted
+    print params_irac
+    print params_nicmos
