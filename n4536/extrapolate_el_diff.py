61766d6645347bc59577b1526d285d0bdda414a3
Michael Williams <mike@pentangle.net>
2012-03-20 18:59:20 +0100

    Significant overhaul to use dummy bulge efit for inward radii

--- n4536/extrapolate_el_diff.py
+++ n4536/extrapolate_el_diff.py
@@ -1,34 +1,38 @@
 import ellipse
-import pylab as p
 import numpy as n
-import os
+import pylab as p
+import copy
 
 d = "/Users/mike/research/2012/smbhpb/data/n4536/profilefit/"
-el_minus_bulge_file = "el_n4536_irac_minus_bulge.ascii"
-el_minus_disk_file = "el_n4536_irac_minus_disk.ascii"
-el_minus_bulge_benderfile = os.path.splitext(el_minus_bulge_file)[0] + ".bender"
-el_minus_disk_benderfile = os.path.splitext(el_minus_disk_file)[0] + ".bender"
-
+el_disk_file = "el_n4536_irac_minus_bulge.ascii"
+el_disk_benderfile = "el_n4536_disk.bender"
+el_bulge_benderfile = "el_n4536_bulge.bender"
 iracpix = 0.75
 iraczp = 18.1639
 
-el_minus_bulge = ellipse.ReadEllipse(d + el_minus_bulge_file, pix = iracpix, ZP = iraczp)
-el_minus_disk = ellipse.ReadEllipse(d + el_minus_disk_file, pix = iracpix, ZP = iraczp)
-p.loglog(el_minus_bulge.a, el_minus_bulge.i, color = "LightSteelBlue")
-p.loglog(el_minus_disk.a, el_minus_disk.i, color = "SandyBrown")
-
-# Extrapolate inwards assuming constant sb
-imax = n.argmax(el_minus_bulge.i)
-el_minus_bulge.i[0:imax] = el_minus_bulge.i[imax]
-el_minus_bulge.sb[0:imax] = el_minus_bulge.sb[imax]
-
+el_disk = ellipse.ReadEllipse(d + el_disk_file, pix = iracpix, ZP = iraczp, dataFrame = False)
 # Get rid of negative counts
-neg = el_minus_bulge.i < 0.
-el_minus_bulge.i[neg] = 1e-4
-el_minus_bulge.sb[neg] = -2.5 * n.log10(el_minus_bulge.i[neg]) + iraczp
+neg = el_disk['intens'] < 0.
+el_disk['intens'][neg] = 1e-4
+el_disk['sb'][neg] = -2.5 * n.log10(el_disk['intens'][neg]) + iraczp
 
-p.loglog(el_minus_bulge.a[0:imax], el_minus_bulge.i[0:imax], "o", color = "LightSteelBlue")
-p.show()
+# Use bulge as dummy efit objects with correct radii
+# Replace bulge with dummy SB and ell values
+el_inner_disk = ellipse.ReadBenderEllipse(d + el_bulge_benderfile)
+nPts = len(el_inner_disk['a'])
+el_inner_disk['eps'][:] = el_disk['ellip'][0]
+el_inner_disk['pa'] = n.zeros(nPts)
+el_inner_disk['sb'] = -2.5 * n.log10(el_disk['intens'].max()) + iraczp
+# TODO: deal with deps, dpa, a4
 
-el_minus_bulge_bender = ellipse.ConvertIrafToBender(el_minus_bulge, dataFrame =  False)
-ellipse.WriteBenderEllipse(el_minus_bulge_bender, d + el_minus_bulge_benderfile)
+el_inner_disk = ellipse.ConvertBenderToIraf(el_inner_disk, dataFrame = False)
+
+# Get radius at which maximum SB occurs in disk efit
+rmax = el_disk['sma'][n.argmax(el_disk['intens'])]
+
+# TODO: Merge el_disk and el_inner_disk at rmax
+# Inspect around rmax to check it's not too jumpy
+# Read IN bender ellipse files and plot to check no basic errors
+
+el_disk_bender = ellipse.ConvertIrafToBender(el_disk, dataFrame =  False, irafColnames = [])
+ellipse.WriteBenderEllipse(el_disk_bender, d + el_disk_benderfile)
